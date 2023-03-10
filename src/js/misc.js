e737c9073023d8b264302d6b16c18e354f801776
gammahazard <92896466+gammahazard@users.noreply.github.com>
2023-03-09 19:45:05 -0500

    add port

--- src/js/misc.js
+++ src/js/misc.js
@@ -43,7 +43,7 @@ contactForm.addEventListener('submit', function(event) {
   submitButton.disabled = true;
   submitButton.textContent = 'Loading...';
 
-  fetch('https://backend-profile-emailer.herokuapp.com/sendMail', {
+  fetch('https://backend-profile-emailer.herokuapp.com:3000/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name, email, message })
